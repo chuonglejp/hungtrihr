9994c5009242c935350442ae67bfc3db88bc6a24
beel <chuong.le.jp@gmail.com>
2022-04-08 09:58:50 +0900

    deploy

---
+++ precache-manifest.abe922a464479c229cff62e6176c581d.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dbcc039e1fd65f395ec",
+    "url": "/css/app.6097604a.css"
+  },
+  {
+    "revision": "1dbcc039e1fd65f395ec",
+    "url": "/js/app.68a185f3.js"
+  },
+  {
+    "revision": "54706f8982c2c629a1e9",
+    "url": "/css/chunk-4e0b8328.1fead564.css"
+  },
+  {
+    "revision": "54706f8982c2c629a1e9",
+    "url": "/js/chunk-4e0b8328.6fe5e35b.js"
+  },
+  {
+    "revision": "78013fb71b8f4698e513",
+    "url": "/js/chunk-52e4de86.fb4a6411.js"
+  },
+  {
+    "revision": "263e0ceb74e98c1ed7da",
+    "url": "/js/chunk-vendors.84a7c646.js"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3c095222ef672d4b0b466ec26c28282a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "59dca61ec2d9d71e3ccee1e9154fa967",
+    "url": "/img/homepage/duhocsinh.jpg"
+  },
+  {
+    "revision": "f33a55641a9989576945785989ae1d4d",
+    "url": "/img/homepage/kysu.jpg"
+  },
+  {
+    "revision": "522364e2f42fd32a243f83f322f282a6",
+    "url": "/img/homepage/thuctapsinh.jpg"
+  },
+  {
+    "revision": "b9934d0b90064587f5491b9965d5ad6c",
+    "url": "/img/logo/gold.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "2a7c4b76d08d48206cb3264e117dd4d9",
+    "url": "/img/logo/Hưng Trí-01.jpg"
+  },
+  {
+    "revision": "e6c2da7f0fa57fd83b65b256a58ecbbf",
+    "url": "/img/logo/Hưng Trí-02.png"
+  },
+  {
+    "revision": "f269cfc7941b413320e89f880c01b3da",
+    "url": "/img/logo/Hưng Trí-03.png"
+  },
+  {
+    "revision": "4f818af6e23bb8e456a4f2e946a60d54",
+    "url": "/img/logo/Hưng Trí-06.jpg"
+  },
+  {
+    "revision": "9238daf2c902152c7a93379a95be0755",
+    "url": "/img/logo/Hưng Trí-05.png"
+  },
+  {
+    "revision": "5dd6ec0a6dc2db8aa8923b39cccf44ef",
+    "url": "/img/logo/Hưng Trí-05.jpg"
+  },
+  {
+    "revision": "c233f0195fd4cc8b27654345179c772c",
+    "url": "/img/logo/Hưng Trí-02.jpg"
+  },
+  {
+    "revision": "5a9ea9073b089f2ff7a56e283752c905",
+    "url": "/img/logo/Hưng Trí-06.png"
+  },
+  {
+    "revision": "37419e384d4833793b75b3816092194f",
+    "url": "/img/logo/Hưng Trí-04.jpg"
+  },
+  {
+    "revision": "b93c6abd26aefb05eae0e22cfba22993",
+    "url": "/img/logo/Hưng Trí-03.jpg"
+  },
+  {
+    "revision": "1c6900bc9ba2fb348b6640a4ef7b3ea6",
+    "url": "/img/logo/logo1.png"
+  },
+  {
+    "revision": "778098cd94fcab7d9af7ade9dc56b2ae",
+    "url": "/img/logo/web.png"
+  },
+  {
+    "revision": "1789a8f64672324ecb2ea586ba7524c4",
+    "url": "/img/logo/white-full.png"
+  },
+  {
+    "revision": "820fe4876d84a46873bfc5509e1f9b9c",
+    "url": "/img/logo/white.png"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "be004b147728ff7d73d37504ce597c0d",
+    "url": "/img/logo/Hưng Trí.pdf"
+  },
+  {
+    "revision": "3494db30404dd43d0478e3fb180959f5",
+    "url": "/img/logo/Hưng Trí.ai"
+  },
+  {
+    "revision": "24b7976a4e7087243015ab21483f4f13",
+    "url": "/img/logo/mockup.png"
+  }
+];
