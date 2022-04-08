4ca12b394feb4df3f944345d9698d51983320a82
C. LE <chuong.le.jp@gmail.com>
2022-04-08 09:46:31 +0900

    fix company name

--- src/db/GioiThieu.js
+++ src/db/GioiThieu.js
@@ -1,11 +1,11 @@
 export const GioiThieuText = {
     gioiThieu: {
         vi: `Công ty Cổ phần Cung ứng Nhân lực Hưng Trí được thành lập với mục địch giải quyết nhu cầu việc làm cho người Việt và vấn đề thiếu hụt nhân lực của các doanh nghiệp Nhật Bản. Với đội ngũ cán bộ nhân viên là những người đã từng sinh sống, học tập hoặc làm việc tại Nhật Bản, chúng tôi thấu hiểu những vấn đề khó khăn, trở ngại mà các công ty hoặc người lao động gặp phải. Chúng tôi sẽ nỗ lực để hoàn thiện nâng cao chất lượng dịch vụ của công ty và sẽ hỗ trợ tận tình các vấn đề người lao động gặp phải trong quá trình trước và sau khi đến Nhật Bản.`,
-        ja: `日本の中小企業が直面している人手不足の問題を解決することを目的として、HUNG TRI株式会社が設立されました。<br>日本とベトナムの間にはいくつかの類似点がありますが、いくつかの違いもあります。 そのため、HR Labcoopは、日本の文化、習慣、企業経営について徹底的にトレーニングすることで、日本で働く意欲のある人材を提供しています...`
+        ja: `日本の中小企業が直面している人手不足の問題を解決することを目的として、フゥン.チイー人材供給株式会社が設立されました。<br>日本とベトナムの間にはいくつかの類似点がありますが、いくつかの違いもあります。 そのため、フゥン.チイー人材供給株式会社は、日本の文化、習慣、企業経営について徹底的にトレーニングすることで、日本で働く意欲のある人材を提供しています...`
     },
     tenCongTy: {
         vi: `Công ty Cổ phần Cung ứng Nhân lực Hưng Trí`,
-        ja: `HUNG TRI株式会社`,
+        ja: `フゥン.チイー人材供給株式会社`,
     },
     email: {
         vi: `thanhhungskv@gmail.com`,
